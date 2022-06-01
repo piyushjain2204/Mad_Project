f924916ac0ede388f6bfa867ed1eb773b49ba811
piyus <201b177@juetguna.in>
2022-06-01 22:06:24 +0530

    Upload Files

---
+++ app/src/main/java/com/example/memory_game/MainActivity.java
@@ -0,0 +1,223 @@
+package com.example.memory_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv_p1, tv_p2;
+
+    ImageView iv_11, iv_12, iv_13, iv_14, iv_21, iv_22, iv_23, iv_24,iv_31, iv_32, iv_33, iv_34;
+
+    //array for the images
+    Integer[] cardArray = { 101, 102, 103, 104, 105, 106, 201, 202, 203, 204, 205, 206};
+
+    //actual images
+    int image101, image102,image103, image104,image105, image106,
+            image201, image202,image203, image204,image205, image206;
+
+    int firstCard, secondCard;
+    int clickedFirst, getClickedSecond;
+    int cardNumber = 1;
+
+    int turn = 1;
+    int playerPoints = 0, cpuPoints = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_p1 = (TextView) findViewById(R.id.tv_p1);
+        tv_p2 = (TextView) findViewById(R.id.tv_p2);
+
+        iv_11 = (ImageView) findViewById(R.id.iv_11);
+        iv_12 = (ImageView) findViewById(R.id.iv_12);
+        iv_13 = (ImageView) findViewById(R.id.iv_13);
+        iv_14 = (ImageView) findViewById(R.id.iv_11);
+        iv_21 = (ImageView) findViewById(R.id.iv_21);
+        iv_22 = (ImageView) findViewById(R.id.iv_22);
+        iv_23 = (ImageView) findViewById(R.id.iv_23);
+        iv_24 = (ImageView) findViewById(R.id.iv_11);
+        iv_31 = (ImageView) findViewById(R.id.iv_31);
+        iv_32 = (ImageView) findViewById(R.id.iv_32);
+        iv_33 = (ImageView) findViewById(R.id.iv_33);
+        iv_34 = (ImageView) findViewById(R.id.iv_34);
+
+        iv_11.setTag("0");
+        iv_12.setTag("1");
+        iv_13.setTag("2");
+        iv_14.setTag("3");
+        iv_21.setTag("4");
+        iv_22.setTag("5");
+        iv_23.setTag("6");
+        iv_24.setTag("7");
+        iv_31.setTag("8");
+        iv_32.setTag("9");
+        iv_33.setTag("10");
+        iv_34.setTag("11");
+
+        //load the card images
+        frontOfCardsResources();
+
+        //shuffle the images
+        Collections.shuffle(Arrays.asList(cardArray));
+
+        //Changing the color of the inactive colour
+        tv_p2.setTextColor(Color.GRAY);
+
+        iv_11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_11, theCard);
+            }
+        });
+
+        iv_12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_12, theCard);
+            }
+        });
+
+        iv_13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_13, theCard);
+            }
+        });
+
+        iv_14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_14, theCard);
+            }
+        });
+
+        iv_21.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_21, theCard);
+            }
+        });
+
+        iv_22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_22, theCard);
+            }
+        });
+
+        iv_23.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_23, theCard);
+            }
+        });
+
+        iv_24.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_24, theCard);
+            }
+        });
+
+        iv_31.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_31, theCard);
+            }
+        });
+
+        iv_32.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_32, theCard);
+            }
+        });
+
+        iv_33.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_33, theCard);
+            }
+        });
+
+        iv_34.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int theCard = Integer.parseInt((String) view.getTag());
+                doStuff(iv_34, theCard);
+            }
+        });
+    }
+
+    private void doStuff(ImageView iv, int card){
+
+        //setting the correct image to the imageview
+        if(cardArray[card] == 101){
+            iv.setImageResource(image101);
+        } else if(cardArray[card] == 102){
+            iv.setImageResource(image102);
+        } else if(cardArray[card] == 103){
+            iv.setImageResource(image103);
+        } else if(cardArray[card] == 104){
+            iv.setImageResource(image104);
+        } else if(cardArray[card] == 105){
+            iv.setImageResource(image105);
+        } else if(cardArray[card] == 106){
+            iv.setImageResource(image106);
+        } else if(cardArray[card] == 201){
+            iv.setImageResource(image201);
+        } else if(cardArray[card] == 202){
+            iv.setImageResource(image202);
+        } else if(cardArray[card] == 203){
+            iv.setImageResource(image203);
+        } else if(cardArray[card] == 204){
+            iv.setImageResource(image204);
+        } else if(cardArray[card] == 205){
+            iv.setImageResource(image205);
+        } else if(cardArray[card] == 206){
+            iv.setImageResource(image206);
+        }
+
+        if(cardNumber == 1) {
+
+        }
+    }
+
+    private void frontOfCardsResources() {
+        image101 = R.drawable.ic_image101;
+        image102 = R.drawable.ic_image102;
+        image103 = R.drawable.ic_image103;
+        image104 = R.drawable.ic_image104;
+        image105 = R.drawable.ic_image105;
+        image106 = R.drawable.ic_image106;
+        image201 = R.drawable.ic_image201;
+        image202 = R.drawable.ic_image202;
+        image203 = R.drawable.ic_image203;
+        image204 = R.drawable.ic_image204;
+        image205 = R.drawable.ic_image205;
+        image206 = R.drawable.ic_image206;
+    }
+}
